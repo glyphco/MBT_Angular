59238725cdf890e3eb6b798765bac201b97350d4
Carleton Maybell <cmaybell@aroptions.com>
2017-06-13 15:23:12 -0500

    Adds moment js to project.

--- src/app/_components/dashboard.component.ts
+++ src/app/_components/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment'; // add this 1 of 4
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent {
 
-  constructor(){}
+  constructor(){
+    let now = moment(); // add this 2 of 4
+    console.log('hello world', now.format()); // add this 3 of 4
+    console.log(now.add(7, 'days').format()); // add this 4of 4
+  }
 }
