a0a019e648380c324ebb87928525c929cd3d40db
Carleton Maybell <cmaybell@aroptions.com>
2017-06-16 17:02:51 -0500

    Google map features work.

--- src/app/_components/venue-create.component.ts
+++ src/app/_components/venue-create.component.ts
@@ -30,6 +30,10 @@ export class VenueCreateComponent implements OnInit {
   venue = new Venue;
   googleAddress = new googleAddress;
   pacInput = '';
+  map:any;
+  marker:any;
+  infowindow:any;
+  infowindowContent:any;
 
   constructor(
     private venueService:VenueService,
@@ -38,50 +42,11 @@ export class VenueCreateComponent implements OnInit {
   ){}
 
   ngOnInit():void{
-    /*
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: {lat: 41.85, lng: -87.65}
-    });
-    directionsDisplay.setMap(map);
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-
-    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-
-        var waypts = [];
-        var checkboxArray:any[] = [
-            'winnipeg', 'regina','calgary'
-    ];
-    for (var i = 0; i < checkboxArray.length; i++) {
-
-          waypts.push({
-            location: checkboxArray[i],
-            stopover: true
-          });
-
-      }
-
-      directionsService.route({
-        origin: {lat: 41.85, lng: -87.65},
-        destination: {lat: 49.3, lng: -123.12},
-        waypoints: waypts,
-        optimizeWaypoints: true,
-        travelMode: 'DRIVING'
-      }, function(response, status) {
-        if (status === 'OK') {
-          directionsDisplay.setDirections(response);
-        } else {
-          window.alert('Directions request failed due to ' + status);
-        }
-      });
-    }*/
-
+    let self = this;
     var origin = {lat: 41.94, lng: -87.68};
     var REQUIRED_ZOOM = 15;
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    this.map = new google.maps.Map(document.getElementById('map'), {
       center: origin,
       zoom: 13
     });
@@ -90,7 +55,7 @@ export class VenueCreateComponent implements OnInit {
     var input = document.getElementById('pac-input');
     var types = document.getElementById('type-selector');
     var strictBounds = document.getElementById('strict-bounds-selector');
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+    this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
     var autocomplete = new google.maps.places.Autocomplete(input);
     var placeSearch;
@@ -106,22 +71,22 @@ export class VenueCreateComponent implements OnInit {
     // Bind the map's bounds (viewport) property to the autocomplete object,
     // so that the autocomplete requests use the current map bounds for the
     // bounds option in the request.
-    autocomplete.bindTo('bounds', map);
+    autocomplete.bindTo('bounds', this.map);
 
-    var infowindow = new google.maps.InfoWindow();
-    var infowindowContent = document.getElementById('infowindow-content');
-    infowindow.setContent(infowindowContent);
-    var marker = new google.maps.Marker({
-      map: map,
+    this.infowindow = new google.maps.InfoWindow();
+    this.infowindowContent = document.getElementById('infowindow-content');
+    this.infowindow.setContent(this.infowindowContent);
+    this.marker = new google.maps.Marker({
+      map: self.map,
       anchorPoint: new google.maps.Point(1, 1)
     });
 
     autocomplete.addListener('place_changed', function()
     {
-      infowindow.close();
-      marker.setVisible(false);
+      self.infowindow.close();
+      self.marker.setVisible(false);
       var place = autocomplete.getPlace();
-      fillInAddressFromPlace(place);
+      self.fillInAddressFromPlace(place);
       if (!place.geometry) {
         // User entered the name of a Place that was not suggested and
         // pressed the Enter key, or the Place Details request failed.
@@ -131,12 +96,12 @@ export class VenueCreateComponent implements OnInit {
 
       // If the place has a geometry, then present it on a map.
       if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
+        self.map.fitBounds(place.geometry.viewport);
       } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);  // Why 17? Because it looks good.
+        self.map.setCenter(place.geometry.location);
+        self.map.setZoom(17);  // Why 17? Because it looks good.
       }
-      marker.setPosition(place.geometry.location);
+      self.marker.setPosition(place.geometry.location);
       //marker.setVisible(true);
 
       var address = '';
@@ -148,144 +113,177 @@ export class VenueCreateComponent implements OnInit {
         ].join(' ');
       }
 
-      infowindowContent.children['place-icon'].src = place.icon;
-      infowindowContent.children['place-name'].textContent = place.name;
-      infowindowContent.children['place-address'].textContent = address;
-      infowindow.setPosition(place.geometry.location)
-      infowindow.open(map);
-        marker.setVisible(false);
+      self.infowindowContent.children['place-icon'].src = place.icon;
+      self.infowindowContent.children['place-name'].textContent = place.name;
+      self.infowindowContent.children['place-address'].textContent = address;
+      self.infowindow.setPosition(place.geometry.location)
+      self.infowindow.open(self.map);
+      self.marker.setVisible(false);
     });
 
-        map.addListener("click", function (event)
-        {
-            var latitude = event.latLng.lat();
-            var longitude = event.latLng.lng();
-            var geocoder = new google.maps.Geocoder;
+    this.map.addListener("click", function (event)
+    {
+        var latitude = event.latLng.lat();
+        var longitude = event.latLng.lng();
+        var geocoder = new google.maps.Geocoder;
 
-            if (event.placeId) {
-              console.log('place:'+event.placeId);
-              getPlaceFromID(event.placeId);
-            } else {
-              console.log('geo:'+event.latLng);
-              geocodeLatLng(geocoder, map, event.latLng);
-            }
-
-        });
-
-      function getPlaceFromID(place_id) {
-        var service = new google.maps.places.PlacesService(map);
-        service.getDetails({
-          placeId: place_id
-        }, function(place, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            fillInAddressFromPlace(place);
-
-            if (!place.geometry) {
-              // User entered the name of a Place that was not suggested and
-              // pressed the Enter key, or the Place Details request failed.
-              window.alert("No details available for input: '" + place.name + "'");
-              return;
-            }
-
-            // If the place has a geometry, then present it on a map.
-            if (place.geometry.viewport) {
-              map.fitBounds(place.geometry.viewport);
-            } else {
-              map.setCenter(place.geometry.location);
-              map.setZoom(16);  // Why 17? Because it looks good.
-            }
-
-            marker.setPosition(place.geometry.location);
-            //marker.setVisible(true);
-
-            var address = '';
-            if (place.address_components) {
-              address = [
-                (place.address_components[0] && place.address_components[0].short_name || ''),
-                (place.address_components[1] && place.address_components[1].short_name || ''),
-                (place.address_components[2] && place.address_components[2].short_name || '')
-              ].join(' ');
-            }
-
-            infowindowContent.children['place-icon'].src = place.icon;
-            infowindowContent.children['place-name'].textContent = place.name;
-            infowindowContent.children['place-address'].textContent = address;
-          infowindow.setPosition(place.geometry.location)
-          infowindow.open(map);
-           marker.setVisible(false);
-          } else {
-            //console.log(status);
-          }
-
-        });
-      }
-
-        function fillInAddressFromPlace(result) {
-          var lockElements = [
-            'name',
-            'street_address',
-            'city',
-            'state',
-            'postalcode',
-            'lat',
-            'lng',
-            'google_place_id',
-          ];
-          var unlockElements = [
-            'neighborhood',
-            'website',
-            'phone',
-            'email',
-            'submit',
-            'image',
-            'background'
-          ];
-          for (var element in lockElements) {
-            //document.getElementById(element).value = '';
-            this.googleAddress[element] = '';
-          }
-
-          for (var element in unlockElements) {
-            //document.getElementById(element).value = '';
-            this.googleAddress[element] = '';
-          }
-          //document.getElementById('submit').value = 'Submit';
-          //console.log(result.address_components);
-
-          var longcomponents=<any>[];
-          var shortcomponents=<any>[];
-          for (var i = 0; i < result.address_components.length; i++) {
-              longcomponents[result.address_components[i]['types'][0]] = result.address_components[i]['long_name'];
-              shortcomponents[result.address_components[i]['types'][0]] = result.address_components[i]['short_name'];
-          }
-
-          this.googleAddress.name = this.coalesce(result.name,'');
-          this.googleAddress.street_address = this.coalesce(longcomponents.street_number + ' ' + shortcomponents.route),'';
-          this.googleAddress.city = this.coalesce(longcomponents.locality,'');
-          this.googleAddress.state = this.coalesce(shortcomponents.administrative_area_level_1,'');
-          this.googleAddress.postalcode = this.coalesce(shortcomponents.postal_code,'');
-          this.googleAddress.lat = this.coalesce(result.geometry.location.lat(),'');
-          this.googleAddress.lng = this.coalesce(result.geometry.location.lng(),'');
-
-          this.googleAddress.neighborhood = this.coalesce(longcomponents.neighborhood,'');
-          this.googleAddress.website = this.coalesce(result.website,'');
-          this.googleAddress.google_place_id = this.coalesce(result.id,'');
-          this.googleAddress.phone = this.coalesce(result.phone,'');
-          this.googleAddress.email = this.coalesce(result.email,'');
-
-          for (var element in lockElements) {
-            (<HTMLInputElement>document.getElementById(element)).readOnly = true;
-            document.getElementById(element).style.backgroundColor = "#999";
-          }
-
-          for (var element in unlockElements) {
-            (<HTMLInputElement>document.getElementById(element)).readOnly = false;
-          }
-
+        if (event.placeId) {
+          console.log('place:'+event.placeId);
+          self.getPlaceFromID(event.placeId, self);
+        } else {
+          console.log('geo:'+event.latLng);
+          self.geocodeLatLng(geocoder, self.map, event.latLng, self);
         }
+
+    });
   }
   
+  public getPlaceFromID(place_id, self) {
+    var service = new google.maps.places.PlacesService(self.map);
+    service.getDetails({
+      placeId: place_id
+    }, function(place, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        self.fillInAddressFromPlace(place);
 
+        if (!place.geometry) {
+          // User entered the name of a Place that was not suggested and
+          // pressed the Enter key, or the Place Details request failed.
+          window.alert("No details available for input: '" + place.name + "'");
+          return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+          self.map.fitBounds(place.geometry.viewport);
+        } else {
+          self.map.setCenter(place.geometry.location);
+          self.map.setZoom(16);  // Why 17? Because it looks good.
+        }
+
+        self.marker.setPosition(place.geometry.location);
+        //marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+          address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+        }
+
+        self.infowindowContent.children['place-icon'].src = place.icon;
+        self.infowindowContent.children['place-name'].textContent = place.name;
+        self.infowindowContent.children['place-address'].textContent = address;
+        self.infowindow.setPosition(place.geometry.location)
+        self.infowindow.open(self.map);
+        self.marker.setVisible(false);
+      } else {
+        //console.log(status);
+      }
+
+    });
+  }
+
+  public geocodeLatLng(geocoder, map, latlng, self) {
+    geocoder.geocode({'location': latlng}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          let placename = results[0].formatted_address;
+          self.fillInAddressFromGeo(results[0]);
+          (<HTMLInputElement>document.getElementById('pac-input')).value = placename;
+        } else {
+            window.alert('No results found');
+        }
+        map.setCenter(results[0].geometry.location);
+        if (map.getZoom() < 16) {
+          map.setZoom(16);  // Why 16? Because it looks good.
+        }
+
+        var address = '';
+        if (results[0].address_components) {
+          address = [
+            (results[0].address_components[0] && results[0].address_components[0].short_name || ''),
+            (results[0].address_components[1] && results[0].address_components[1].short_name || ''),
+            (results[0].address_components[2] && results[0].address_components[2].short_name || '')
+          ].join(' ');
+        }
+
+        self.infowindowContent.children['place-icon'].src = '';
+        self.infowindowContent.children['place-name'].textContent = 'unknown';
+        self.infowindowContent.children['place-address'].textContent = address;
+        self.infowindow.setPosition(results[0].geometry.location)
+        self.infowindow.open(map);
+        self.marker.setVisible(false);
+      } else {
+      window.alert('Geocoder failed due to: ' + status);
+      }
+    });
+  }
+
+  public fillInAddressFromPlace(result) {
+    var lockElements = [
+      'name',
+      'street_address',
+      'city',
+      'state',
+      'postalcode',
+      'lat',
+      'lng',
+      'google_place_id',
+    ];
+    var unlockElements = [
+      'neighborhood',
+      'website',
+      'phone',
+      'email',
+      'submit',
+      'image',
+      'background'
+    ];
+    for (var element in lockElements) {
+      //document.getElementById(element).value = '';
+      this.googleAddress[element] = '';
+    }
+
+    for (var element in unlockElements) {
+      //document.getElementById(element).value = '';
+      this.googleAddress[element] = '';
+    }
+    //document.getElementById('submit').value = 'Submit';
+    //console.log(result.address_components);
+
+    var longcomponents=<any>[];
+    var shortcomponents=<any>[];
+    for (var i = 0; i < result.address_components.length; i++) {
+        longcomponents[result.address_components[i]['types'][0]] = result.address_components[i]['long_name'];
+        shortcomponents[result.address_components[i]['types'][0]] = result.address_components[i]['short_name'];
+    }
+
+    this.googleAddress.name = this.coalesce(result.name,'');
+    this.googleAddress.street_address = this.coalesce(longcomponents.street_number + ' ' + shortcomponents.route),'';
+    this.googleAddress.city = this.coalesce(longcomponents.locality,'');
+    this.googleAddress.state = this.coalesce(shortcomponents.administrative_area_level_1,'');
+    this.googleAddress.postalcode = this.coalesce(shortcomponents.postal_code,'');
+    this.googleAddress.lat = this.coalesce(result.geometry.location.lat(),'');
+    this.googleAddress.lng = this.coalesce(result.geometry.location.lng(),'');
+
+    this.googleAddress.neighborhood = this.coalesce(longcomponents.neighborhood,'');
+    this.googleAddress.website = this.coalesce(result.website,'');
+    this.googleAddress.google_place_id = this.coalesce(result.id,'');
+    this.googleAddress.phone = this.coalesce(result.phone,'');
+    this.googleAddress.email = this.coalesce(result.email,'');
+    /*
+    for (var element in lockElements) {
+      (<HTMLInputElement>document.getElementById(element)).readOnly = true;
+      document.getElementById(element).style.backgroundColor = "#999";
+    }
+
+    for (var element in unlockElements) {
+      (<HTMLInputElement>document.getElementById(element)).readOnly = false;
+    }*/
+  }
   
   public fillInAddressFromGeo(result) {
       // Get the place details from the autocomplete object.
@@ -309,6 +307,7 @@ export class VenueCreateComponent implements OnInit {
       'image',
       'background'
     ];
+    /*
     for (var element in lockElements) {
       (<HTMLInputElement>document.getElementById(element)).value = '';
       (<HTMLInputElement>document.getElementById(element)).readOnly = true;
@@ -319,6 +318,8 @@ export class VenueCreateComponent implements OnInit {
       (<HTMLInputElement>document.getElementById(element)).value = '';
       (<HTMLInputElement>document.getElementById(element)).readOnly = false;
     }
+    */
+
     //document.getElementById('submit').value = 'Submit';
     //console.log(result.address_components);
 
