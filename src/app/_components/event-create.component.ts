e9006d6e3f24e15e3363010c82d31b655fba1fb5
Carleton Maybell <cmaybell@aroptions.com>
2017-07-13 17:12:48 -0500

    Enhances venue selection on event create page.

--- src/app/_components/event-create.component.ts
+++ src/app/_components/event-create.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import { StatesHelper } from '../_helpers/states-helper';
 import { SearchService } from '../_services/search.service';
 import { Event } from '../_models/event';
 import { Venue } from '../_models/venue';
@@ -16,28 +17,40 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
 
+declare var google:any;
+
 @Component({
   selector: 'app-event-create',
   templateUrl: './event-create.component.html',
-  styleUrls: ['./event-create.component.css','./modal.component.css']
+  styleUrls: ['./modal.component.css','./event-create.component.css']
 })
 export class EventCreateComponent implements OnInit {
-  event = new Event;
+  event = new Event();
   venue:Venue;
+  tempVenue:Venue; //used for creating a custom venue
+  states = StatesHelper.states;
   venueModalVisible = false;
   private searchTerms = new Subject<string>();
   venueResults: Observable<any[]>;
+  geocoder:any;
+  venueGeocodeResults = [];
+  resultMapping:{[k: string]: string} = {'=0': '0 results.', '=1': '1 result.', 'other': '# results.'};
+  venueResultError = false;
 
   constructor(
     private eventService:EventService,
     private route: ActivatedRoute,
     private location: Location,
-    private searchService: SearchService
+    private searchService: SearchService,
+    private _ngZone: NgZone
   ){
     this.event.startTime = '20:00';
   }
 
   ngOnInit():void {
+    //Google map stuff
+    this.geocoder = new google.maps.Geocoder;
+    //live search for venues
     this.venueResults = this.searchTerms
       .debounceTime(300)        // wait 300ms after each keystroke before considering the term
       .distinctUntilChanged()   // ignore if next search term is same as previous
@@ -59,6 +72,26 @@ export class EventCreateComponent implements OnInit {
   public chooseVenue(venue: any){
     this.venue = Venue.map(venue);
     this.venueModalVisible = false;
+    this.venueResults = Observable.of<any[]>([]);
+  }
+
+  public addManualVenue(){
+    //this.venueModalVisible = false;
+    this.tempVenue = new Venue();
+  }
+
+  public manualVenueSubmit(){
+    //remove errors
+    this.venueResultError = false;
+    let address = `${this.tempVenue.streetAddress || ''}, ${this.tempVenue.city || ''}, ${this.tempVenue.state || ''} ${this.tempVenue.postalCode || ''}`;
+    console.log(address);
+    //TODO: take out this debug code
+    //let address = "19000 keeler ave, country club hills, IL 60478";
+    this.geocodeAddress(address);
+  }
+
+  public clearVenue(){
+    this.venue = null;
   }
 
   public goBack(): void {
@@ -67,6 +100,30 @@ export class EventCreateComponent implements OnInit {
 
   public onSubmit(){
     this.createEvent();
+  }
+
+  public useManualResult(geocodeObj){
+    let lat = geocodeObj.geometry.location.lat();
+    let lng = geocodeObj.geometry.location.lng();
+    this.tempVenue.lat = lat;
+    this.tempVenue.lng = lng;
+    //save temp venue to real venue
+    this.venue = this.tempVenue;
+    //clear temp venue
+    this.tempVenue = null;
+    //clear results
+    this.venueGeocodeResults = [];
+    this.venueModalVisible = false;
+  }
+
+  private geocodeAddress(address) {
+    this.geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        this._ngZone.run(() => this.venueGeocodeResults = results );
+      } else {
+        this._ngZone.run(() => this.venueResultError = true );
+      }
+    }.bind(this));
   }
 
   private createEvent(){
