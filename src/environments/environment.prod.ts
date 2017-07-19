2bb788d11c0d945b37530756aa5432b8ad5a9644
Carleton Maybell <cmaybell@aroptions.com>
2017-07-19 11:11:59 -0500

    Update env file to curent api and auth server.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  authServer: 'https://mbtauth.dev',
-  apiServer: 'https://mbtapi.dev/api',
+  authServer: 'https://auth.myboringtown.com',
+  apiServer: 'https://api.myboringtown.com/api',
   //within how many minutes should the token be refreshed
   refreshWindow: 30,
   googleTimezoneKey: 'AIzaSyCJ9ag8h4yGT6B_g-giiTk44c2oKh9uLaQ'
