9cf202f751f98acfffec64d81fe8753263ba2ada
AROPTIONS\sdalton <glypher@gmail.com>
2017-09-08 16:12:23 -0500

    removing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,79 +5,79 @@ import { AppComponent } from './app.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { PublicGuard } from './_guards/public.guard';
 
-import { BackstageComponent } from './_components/backstage.component';
-import { LoginComponent } from './_components/login.component';
-import { SearchComponent } from './_components/search.component';
-import { PageDetailComponent } from './_components/page-detail.component';
-import { PageCreateComponent } from './_components/page-create.component';
-import { EventDetailComponent } from './_components/event-detail.component';
-import { EventFullComponent } from './_components/event-full.component';
-import { VenueDetailComponent } from './_components/venue-detail.component';
-import { VenueCreateComponent } from './_components/venue-create.component';
-import { ShowDetailComponent } from './_components/show-detail.component';
-import { ShowEditComponent } from './_components/show-edit.component';
-import { ShowDeleteComponent } from './_components/show-delete.component';
-import { ShowCreateComponent } from './_components/show-create.component';
-import { ShowsEditableComponent } from './_components/shows-editable.component';
-import { PageEditComponent } from './_components/page-edit.component';
-import { PageDeleteComponent } from './_components/page-delete.component';
-import { EventEditComponent } from './_components/event-edit.component';
-import { EventDeleteComponent } from './_components/event-delete.component';
-import { EventCreateComponent } from './_components/event-create.component';
+// import { BackstageComponent } from './_components/backstage.component';
+// import { LoginComponent } from './_components/login.component';
+// import { SearchComponent } from './_components/search.component';
+// import { PageDetailComponent } from './_components/page-detail.component';
+// import { PageCreateComponent } from './_components/page-create.component';
+// import { EventDetailComponent } from './_components/event-detail.component';
+// import { EventFullComponent } from './_components/event-full.component';
+// import { VenueDetailComponent } from './_components/venue-detail.component';
+// import { VenueCreateComponent } from './_components/venue-create.component';
+// import { ShowDetailComponent } from './_components/show-detail.component';
+// import { ShowEditComponent } from './_components/show-edit.component';
+// import { ShowDeleteComponent } from './_components/show-delete.component';
+// import { ShowCreateComponent } from './_components/show-create.component';
+// import { ShowsEditableComponent } from './_components/shows-editable.component';
+// import { PageEditComponent } from './_components/page-edit.component';
+// import { PageDeleteComponent } from './_components/page-delete.component';
+// import { EventEditComponent } from './_components/event-edit.component';
+// import { EventDeleteComponent } from './_components/event-delete.component';
+// import { EventCreateComponent } from './_components/event-create.component';
 import { PublicEventsTodayComponent } from './_components/public-events-today.component';
-import { EventsTodayComponent } from './_components/events-today.component';
-import { EventsCurrentComponent } from './_components/events-current.component';
-import { ExploreComponent } from './_components/explore.component';
-import { EventsEditableComponent } from './_components/events-editable.component';
-import { PagesEditableComponent } from './_components/pages-editable.component';
-import { VenuesEditableComponent } from './_components/venues-editable.component';
-import { VenueEditComponent } from './_components/venue-edit.component';
-import { VenueDeleteComponent } from './_components/venue-delete.component';
-import { UserDetailComponent } from './_components/user-detail.component';
-import { ProfileEditComponent } from './_components/profile-edit.component';
-import { UsersEditableComponent } from './_components/users-editable.component';
-import { UserEditComponent } from './_components/user-edit.component';
-import { ManageFollowersComponent } from './_components/manage-followers.component';
+// import { EventsTodayComponent } from './_components/events-today.component';
+// import { EventsCurrentComponent } from './_components/events-current.component';
+// import { ExploreComponent } from './_components/explore.component';
+// import { EventsEditableComponent } from './_components/events-editable.component';
+// import { PagesEditableComponent } from './_components/pages-editable.component';
+// import { VenuesEditableComponent } from './_components/venues-editable.component';
+// import { VenueEditComponent } from './_components/venue-edit.component';
+// import { VenueDeleteComponent } from './_components/venue-delete.component';
+// import { UserDetailComponent } from './_components/user-detail.component';
+// import { ProfileEditComponent } from './_components/profile-edit.component';
+// import { UsersEditableComponent } from './_components/users-editable.component';
+// import { UserEditComponent } from './_components/user-edit.component';
+// import { ManageFollowersComponent } from './_components/manage-followers.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent, canActivate: [PublicGuard] },
-  { path: 'backstage', component: BackstageComponent, canActivate: [AuthGuard] },
-  { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
-  { path: 'user/:id', component: UserDetailComponent, canActivate: [AuthGuard] },
-  { path: 'profile/edit', component: ProfileEditComponent, canActivate: [AuthGuard] },
-  //Page routes
-  { path: 'page/create', component: PageCreateComponent, canActivate: [AuthGuard] },
-  { path: 'page/:id', component: PageDetailComponent, canActivate: [AuthGuard] },
-  { path: 'page/edit/:id', component: PageEditComponent, canActivate: [AuthGuard] },
-  { path: 'page/delete/:id', component: PageDeleteComponent, canActivate: [AuthGuard] },
-  { path: 'pages/editable', component: PagesEditableComponent, canActivate: [AuthGuard] },
-  //Venue routes
-  { path: 'venue/create', component: VenueCreateComponent, canActivate: [AuthGuard] },
-  { path: 'venue/:id', component: VenueDetailComponent },
-  { path: 'venues/editable', component: VenuesEditableComponent, canActivate: [AuthGuard] },
-  { path: 'venue/edit/:id', component: VenueEditComponent, canActivate: [AuthGuard] },
-  { path: 'venue/delete/:id', component: VenueDeleteComponent, canActivate: [AuthGuard] },
-  //Event routes
-  { path: 'event/create', component: EventCreateComponent, canActivate: [AuthGuard] },
-  { path: 'event/:id', component: EventDetailComponent, canActivate: [AuthGuard] },
-  { path: 'event/edit/:id', component: EventEditComponent, canActivate: [AuthGuard] },
-  { path: 'event/full/:id', component: EventFullComponent, canActivate: [AuthGuard] },
-  { path: 'event/delete/:id', component: EventDeleteComponent, canActivate: [AuthGuard] },
-  { path: 'events/editable', component: EventsEditableComponent, canActivate: [AuthGuard] },
-  { path: 'events/today', component: EventsTodayComponent, canActivate: [AuthGuard] },
-  { path: 'events/current', component: EventsCurrentComponent, canActivate: [AuthGuard] },
-  //Show routes
-  { path: 'show/create', component: ShowCreateComponent, canActivate: [AuthGuard] },
-  { path: 'show/:id', component: ShowDetailComponent, canActivate: [AuthGuard] },
-  { path: 'show/edit/:id', component: ShowEditComponent, canActivate: [AuthGuard] },
-  { path: 'show/delete/:id', component: ShowDeleteComponent, canActivate: [AuthGuard] },
-  { path: 'shows/editable', component: ShowsEditableComponent, canActivate: [AuthGuard] },
-  //User routes
-  { path: 'users/editable', component: UsersEditableComponent, canActivate: [AuthGuard] },
-  { path: 'user/edit/:id', component: UserEditComponent, canActivate: [AuthGuard] },
-  { path: 'manage/followers', component: ManageFollowersComponent, canActivate: [AuthGuard] },
+  // { path: 'login', component: LoginComponent, canActivate: [PublicGuard] },
+  // { path: 'backstage', component: BackstageComponent, canActivate: [AuthGuard] },
+  // { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
+  // { path: 'user/:id', component: UserDetailComponent, canActivate: [AuthGuard] },
+  // { path: 'profile/edit', component: ProfileEditComponent, canActivate: [AuthGuard] },
+  // //Page routes
+  // { path: 'page/create', component: PageCreateComponent, canActivate: [AuthGuard] },
+  // { path: 'page/:id', component: PageDetailComponent, canActivate: [AuthGuard] },
+  // { path: 'page/edit/:id', component: PageEditComponent, canActivate: [AuthGuard] },
+  // { path: 'page/delete/:id', component: PageDeleteComponent, canActivate: [AuthGuard] },
+  // { path: 'pages/editable', component: PagesEditableComponent, canActivate: [AuthGuard] },
+  // //Venue routes
+  // { path: 'venue/create', component: VenueCreateComponent, canActivate: [AuthGuard] },
+  // { path: 'venue/:id', component: VenueDetailComponent },
+  // { path: 'venues/editable', component: VenuesEditableComponent, canActivate: [AuthGuard] },
+  // { path: 'venue/edit/:id', component: VenueEditComponent, canActivate: [AuthGuard] },
+  // { path: 'venue/delete/:id', component: VenueDeleteComponent, canActivate: [AuthGuard] },
+  // //Event routes
+  // { path: 'event/create', component: EventCreateComponent, canActivate: [AuthGuard] },
+  // { path: 'event/:id', component: EventDetailComponent, canActivate: [AuthGuard] },
+  // { path: 'event/edit/:id', component: EventEditComponent, canActivate: [AuthGuard] },
+  // { path: 'event/full/:id', component: EventFullComponent, canActivate: [AuthGuard] },
+  // { path: 'event/delete/:id', component: EventDeleteComponent, canActivate: [AuthGuard] },
+  // { path: 'events/editable', component: EventsEditableComponent, canActivate: [AuthGuard] },
+  // { path: 'events/today', component: EventsTodayComponent, canActivate: [AuthGuard] },
+  // { path: 'events/current', component: EventsCurrentComponent, canActivate: [AuthGuard] },
+  // //Show routes
+  // { path: 'show/create', component: ShowCreateComponent, canActivate: [AuthGuard] },
+  // { path: 'show/:id', component: ShowDetailComponent, canActivate: [AuthGuard] },
+  // { path: 'show/edit/:id', component: ShowEditComponent, canActivate: [AuthGuard] },
+  // { path: 'show/delete/:id', component: ShowDeleteComponent, canActivate: [AuthGuard] },
+  // { path: 'shows/editable', component: ShowsEditableComponent, canActivate: [AuthGuard] },
+  // //User routes
+  // { path: 'users/editable', component: UsersEditableComponent, canActivate: [AuthGuard] },
+  // { path: 'user/edit/:id', component: UserEditComponent, canActivate: [AuthGuard] },
+  // { path: 'manage/followers', component: ManageFollowersComponent, canActivate: [AuthGuard] },
 
-  { path: 'explore', component: ExploreComponent, canActivate: [AuthGuard] },
+  // { path: 'explore', component: ExploreComponent, canActivate: [AuthGuard] },
   { path: 'events', component: PublicEventsTodayComponent, canActivate: [PublicGuard] },
   
   //{ path: '', redirectTo: 'login', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { MdNativeDateModule, MdDatepickerModule, MdInputModule } from '@angular/material';
-import { FileUploadModule } from 'ng2-file-upload/ng2-file-upload';
+// import { FileUploadModule } from 'ng2-file-upload/ng2-file-upload';
 import { TruncatePipe } from './pipes/truncate.pipe';
 import { DateFormatterPipe } from './pipes/date-formatter.pipe';
 
@@ -15,60 +15,60 @@ import { PublicGuard } from './_guards/public.guard';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './_components/login.component';
-import { BackstageComponent } from './_components/backstage.component';
-import { EventsEditableComponent } from './_components/events-editable.component';
-import { VenuesEditableComponent } from './_components/venues-editable.component';
-import { VenueCreateComponent } from './_components/venue-create.component';
-import { ShowsEditableComponent } from './_components/shows-editable.component';
-import { SearchComponent } from './_components/search.component';
-import { PagesEditableComponent } from './_components/pages-editable.component';
-import { PageDetailComponent } from './_components/page-detail.component';
-import { PageEditComponent } from './_components/page-edit.component';
-import { PageCreateComponent } from './_components/page-create.component';
-import { PageDeleteComponent } from './_components/page-delete.component';
-import { EventDetailComponent } from './_components/event-detail.component';
-import { EventFullComponent } from './_components/event-full.component';
-import { EventDeleteComponent } from './_components/event-delete.component';
-import { VenueDetailComponent } from './_components/venue-detail.component';
-import { VenueEditComponent } from './_components/venue-edit.component';
-import { VenueDeleteComponent } from './_components/venue-delete.component';
-import { ShowDetailComponent } from './_components/show-detail.component';
-import { ShowEditComponent } from './_components/show-edit.component';
-import { ShowDeleteComponent } from './_components/show-delete.component';
-import { ShowCreateComponent } from './_components/show-create.component';
-import { EventEditComponent } from './_components/event-edit.component';
-import { EventCreateComponent } from './_components/event-create.component';
-import { DatepickerComponent } from './_components/datepicker.component';
+// import { BackstageComponent } from './_components/backstage.component';
+// import { EventsEditableComponent } from './_components/events-editable.component';
+// import { VenuesEditableComponent } from './_components/venues-editable.component';
+// import { VenueCreateComponent } from './_components/venue-create.component';
+// import { ShowsEditableComponent } from './_components/shows-editable.component';
+// import { SearchComponent } from './_components/search.component';
+// import { PagesEditableComponent } from './_components/pages-editable.component';
+// import { PageDetailComponent } from './_components/page-detail.component';
+// import { PageEditComponent } from './_components/page-edit.component';
+// import { PageCreateComponent } from './_components/page-create.component';
+// import { PageDeleteComponent } from './_components/page-delete.component';
+// import { EventDetailComponent } from './_components/event-detail.component';
+// import { EventFullComponent } from './_components/event-full.component';
+// import { EventDeleteComponent } from './_components/event-delete.component';
+// import { VenueDetailComponent } from './_components/venue-detail.component';
+// import { VenueEditComponent } from './_components/venue-edit.component';
+// import { VenueDeleteComponent } from './_components/venue-delete.component';
+// import { ShowDetailComponent } from './_components/show-detail.component';
+// import { ShowEditComponent } from './_components/show-edit.component';
+// import { ShowDeleteComponent } from './_components/show-delete.component';
+// import { ShowCreateComponent } from './_components/show-create.component';
+// import { EventEditComponent } from './_components/event-edit.component';
+// import { EventCreateComponent } from './_components/event-create.component';
+// import { DatepickerComponent } from './_components/datepicker.component';
 import { PublicEventsTodayComponent } from './_components/public-events-today.component';
-import { EventsTodayComponent } from './_components/events-today.component';
-import { EventsCurrentComponent } from './_components/events-current.component';
-import { ExploreComponent } from './_components/explore.component';
-import { UserDetailComponent } from './_components/user-detail.component';
-import { CategorySelectComponent } from './_components/category-select.component';
+// import { EventsTodayComponent } from './_components/events-today.component';
+// import { EventsCurrentComponent } from './_components/events-current.component';
+// import { ExploreComponent } from './_components/explore.component';
+// import { UserDetailComponent } from './_components/user-detail.component';
+// import { CategorySelectComponent } from './_components/category-select.component';
 import { LocationPickerComponent } from './_components/location-picker.component';
-import { ProfileEditComponent } from './_components/profile-edit.component';
-import { UsersEditableComponent } from './_components/users-editable.component';
-import { UserEditComponent } from './_components/user-edit.component';
-import { ManageFollowersComponent } from './_components/manage-followers.component';
-import { FollowerRequestsComponent } from './_components/follower-requests.component';
-import { FollowersComponent } from './_components/followers.component';
-import { FollowersBlockedComponent } from './_components/followers-blocked.component';
-import { PyfComponent } from './_components/pyf.component';
-import { ModalComponent } from './_components/modal.component';
+// import { ProfileEditComponent } from './_components/profile-edit.component';
+// import { UsersEditableComponent } from './_components/users-editable.component';
+// import { UserEditComponent } from './_components/user-edit.component';
+// import { ManageFollowersComponent } from './_components/manage-followers.component';
+// import { FollowerRequestsComponent } from './_components/follower-requests.component';
+// import { FollowersComponent } from './_components/followers.component';
+// import { FollowersBlockedComponent } from './_components/followers-blocked.component';
+// import { PyfComponent } from './_components/pyf.component';
+// import { ModalComponent } from './_components/modal.component';
 
 import { AuthService } from './_services/auth.service';
-import { VenueService } from './_services/venue.service';
+// import { VenueService } from './_services/venue.service';
 import { EventService } from './_services/event.service';
 import { SearchService } from './_services/search.service';
 import { HttpHandlerService } from './_services/http-handler.service';
 import { JwtHelperService } from './_services/jwt-helper.service';
-import { PageService } from './_services/page.service';
-import { ShowService } from './_services/show.service';
+// import { PageService } from './_services/page.service';
+// import { ShowService } from './_services/show.service';
 import { CategoryService } from './_services/category.service';
 import { LocationService } from './_services/location.service';
 import { MeService } from './_services/me.service';
-import { UserService } from './_services/user.service';
-import { ImageUploadService } from './_services/image-upload.service';
+// import { UserService } from './_services/user.service';
+// import { ImageUploadService } from './_services/image-upload.service';
 import { SocialLoginService } from './_services/social-login.service';
 import { AnnouncementService } from './_services/announcement.service';
 
@@ -78,46 +78,46 @@ import { AnnouncementService } from './_services/announcement.service';
     DateFormatterPipe,
     AppComponent,
     LoginComponent,
-    BackstageComponent,
-    EventsEditableComponent,
-    VenuesEditableComponent,
-    VenueCreateComponent,
-    VenueEditComponent,
-    VenueDeleteComponent,
-    ShowsEditableComponent,
-    SearchComponent,
-    PagesEditableComponent,
-    PageDetailComponent,
-    EventFullComponent,
-    EventDeleteComponent,
-    PageCreateComponent,
-    PageDeleteComponent,
-    EventDetailComponent,
-    VenueDetailComponent,
-    ShowDetailComponent,
-    ShowEditComponent,
-    ShowDeleteComponent,
-    ShowCreateComponent,
-    PageEditComponent,
-    EventEditComponent,
-    EventCreateComponent,
-    DatepickerComponent,
+    // BackstageComponent,
+    // EventsEditableComponent,
+    // VenuesEditableComponent,
+    // VenueCreateComponent,
+    // VenueEditComponent,
+    // VenueDeleteComponent,
+    // ShowsEditableComponent,
+    // SearchComponent,
+    // PagesEditableComponent,
+    // PageDetailComponent,
+    // EventFullComponent,
+    // EventDeleteComponent,
+    // PageCreateComponent,
+    // PageDeleteComponent,
+    // EventDetailComponent,
+    // VenueDetailComponent,
+    // ShowDetailComponent,
+    // ShowEditComponent,
+    // ShowDeleteComponent,
+    // ShowCreateComponent,
+    // PageEditComponent,
+    // EventEditComponent,
+    // EventCreateComponent,
+    // DatepickerComponent,
     PublicEventsTodayComponent,
-    EventsTodayComponent,
-    EventsCurrentComponent,
-    ExploreComponent,
-    UserDetailComponent,
-    CategorySelectComponent,
+    // EventsTodayComponent,
+    // EventsCurrentComponent,
+    // ExploreComponent,
+    // UserDetailComponent,
+    // CategorySelectComponent,
     LocationPickerComponent,
-    ProfileEditComponent,
-    UsersEditableComponent,
-    UserEditComponent,
-    ManageFollowersComponent,
-    FollowerRequestsComponent,
-    FollowersComponent,
-    FollowersBlockedComponent,
-    PyfComponent,
-    ModalComponent
+    // ProfileEditComponent,
+    // UsersEditableComponent,
+    // UserEditComponent,
+    // ManageFollowersComponent,
+    // FollowerRequestsComponent,
+    // FollowersComponent,
+    // FollowersBlockedComponent,
+    // PyfComponent,
+    // ModalComponent
   ],
   imports: [
     BrowserModule,
@@ -130,24 +130,24 @@ import { AnnouncementService } from './_services/announcement.service';
     MdDatepickerModule,
     MdInputModule,
     ReactiveFormsModule,
-    FileUploadModule
+    // FileUploadModule
   ],
   providers: [
     AuthGuard,
     PublicGuard,
     AuthService,
     HttpHandlerService,
-    VenueService,
+    // VenueService,
     EventService,
     JwtHelperService,
     SearchService,
-    PageService,
-    ShowService,
+    // PageService,
+    // ShowService,
     CategoryService,
     LocationService,
     MeService,
-    UserService,
-    ImageUploadService,
+    // UserService,
+    // ImageUploadService,
     SocialLoginService,
     AnnouncementService
   ],
